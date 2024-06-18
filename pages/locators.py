bb8b97238bd9fc24e81994be6b9ccdc228f1bcd8
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-06-18 13:39:19 +0300

    Добавление локаторов к элементам страницы товаров

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_ADD = (By.CSS_SELECTOR, "div.alertinner")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_BASKET = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
