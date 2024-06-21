10c11b4a4730000cc3a2a9e3e8a60ab7b73e75ea
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-06-21 09:31:41 +0300

    Внесение правок в assert

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,7 +14,7 @@ class ProductPage(BasePage):
         name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
         message_add = self.browser.find_element(*ProductPageLocators.MESSAGE_ADD).text
 
-        assert name_product in message_add, "No product name in the message"
+        assert name_product == message_add, "No product name in the message"
 
     def should_be_message_basket_total(self):
         assert self.is_element_present(*ProductPageLocators.PRICE_PRODUCT), "Product price is not presented"
@@ -23,7 +23,7 @@ class ProductPage(BasePage):
         price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         price_basket = self.browser.find_element(*ProductPageLocators.PRICE_BASKET).text
 
-        assert price_product in price_basket, "No product price in the message" 
+        assert price_product == price_basket, "No product price in the message" 
     
 
 
