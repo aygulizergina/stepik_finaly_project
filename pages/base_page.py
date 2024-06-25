2f92857dd829f74fbabc35e160b4c85ac2e96062
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-06-25 19:32:18 +0300

    Перенос методов из main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,20 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-
+from .locators import BasePageLocators
 import math
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        #self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -18,6 +22,9 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+    
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
