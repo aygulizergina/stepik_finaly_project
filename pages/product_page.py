935d824a029bbd2dad68d3618f2179dd051d0f4a
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-07-11 08:46:06 +0300

    Перенос абстрактных методов отрицательных проверок в base_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,6 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
@@ -27,22 +24,6 @@ class ProductPage(BasePage):
         price_basket = self.browser.find_element(*ProductPageLocators.PRICE_BASKET).text
 
         assert price_product == price_basket, "No product price in the message" 
-
-    
-    def is_not_element_present(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-        return False
-
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-        return True
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
