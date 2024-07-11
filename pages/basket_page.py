a5092c1c99e4cc7f77c9231cb7360f8f25074aba
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-07-11 08:37:16 +0300

    Задание: наследование и отрицательные проверки

---
+++ pages/basket_page.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+class BasketPage(BasePage):
+    def should_be_empty_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.ITEMS_TO_BUY_NOW), "There are items in the basket"
+
+    def should_be_message_empty_basket(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_MESSAGE), "No message that basket is empty"
