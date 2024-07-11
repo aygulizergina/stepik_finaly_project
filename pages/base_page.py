31406f7300917c871c3151a3b095e2140da60748
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-07-11 08:35:27 +0300

    Добавление абстрактных методов для отрицательных проверок

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
 import math
 
@@ -13,8 +16,12 @@ class BasePage():
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
+
+    def go_to_basket_page(self):
+        button_basket = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
+        button_basket.click()
 
     def is_element_present(self, how, what):
         try:
@@ -39,3 +46,18 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
