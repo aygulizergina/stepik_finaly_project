bb0c11c0fb0b514e21492b009d6e11ba57243081
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-07-24 16:08:22 +0300

    Задание: группировка тестов и setup

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
@@ -8,13 +9,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # проверка на корректность url адреса
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is presented"
 
     def should_be_login_form(self):
-        # проверка, что есть форма логина
         assert self.is_element_present(By.CSS_SELECTOR, "#login_form"), "Login form is presented"
 
     def should_be_register_form(self):
-        # проверка, что есть форма регистрации на странице
         assert self.is_element_present(By.CSS_SELECTOR, "#register_form"), "Register form is presented"
+
+    def register_new_user(self, email, password = "12abc345cdf"):
+        self.browser.find_element(*LoginPageLocators.EMAIL_FIELD).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON).click()
+
+
+        
