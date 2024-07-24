5ca0f16093909e658812a1852950781b1ad35152
Айгуль Газизова С. <ai.gazizova@bars.bars-kazan.ru>
2024-07-24 16:10:02 +0300

    Задание: группировка тестов и setup

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,22 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.XPATH, "//span[@class='btn-group']/a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators:
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner")
+    ITEMS_TO_BUY_NOW = (By.CSS_SELECTOR, ".basket-items")
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_FIELD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_FIELD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (By.NAME, "registration_submit")
 
 class ProductPageLocators():
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -12,11 +26,3 @@ class ProductPageLocators():
     PRICE_BASKET = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     SUCCESS_MESSAGE=(By.CSS_SELECTOR,"div.alertinner")
 
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_BUTTON = (By.XPATH, "//span[@class='btn-group']/a")
-
-class BasketPageLocators:
-    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner")
-    ITEMS_TO_BUY_NOW = (By.CSS_SELECTOR, ".basket-items")
